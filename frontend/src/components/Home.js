49b8c8c1e21402b70ed59f7a959a687763b24f8c
jkof86 <cisjay@gmail.com>
2024-08-15 07:24:11 -0400

    Fixed Mui bugs (conflicting node_modules folder in parent directory)

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,7 +3,14 @@ import { Box } from "@mui/material";
 export default function Home() {
 
     return (<>
+        <Box component="section" sx={{
+            textAlign: 'center', 
+            backgroundColor: 'grey',
+            borderRadius: '25px',
+            border: '3px solid white',
+            margin: '10px',
+            padding: '10px' }}>
             <h1>Nutrition App</h1>
-            <h2>Home Page</h2>
+        </Box>
     </>);
 }
