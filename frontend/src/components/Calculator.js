a390ef1a52ec292af99abf251fcec0ad34f0e1cb
jkof86 <cisjay@gmail.com>
2024-08-16 15:26:20 -0400

    Added Calculator Component and established Route

---
+++ frontend/src/components/Calculator.js
@@ -0,0 +1,21 @@
+import { Box } from "@mui/material";
+
+export default function Calculator() {
+
+    return (<>
+    <center>
+        <Box component="section" sx={{
+            textAlign: 'center', 
+            justifyContent: 'center',
+            backgroundColor: 'grey',
+            borderRadius: '25px',
+            border: '1px solid black',
+            boxShadow: '0px 0px 2px 2px white',
+            margin: '10px',
+            padding: '10px',
+            width: '50%' }}>
+            <h1>Calorie / Macronutrient Calculator</h1>
+        </Box>
+        </center>
+    </>);
+}
--- frontend/src/components/navigation/NavDrawer.js
+++ frontend/src/components/navigation/NavDrawer.js
@@ -11,141 +11,152 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
 
-
-const navItems = ['Home', 'About', 'Contact Us'];
+const navItems = ['Home', 'Calculator', 'About', 'Contact Us'];
 const navItems2 = ['Account', 'Settings', 'Logout'];
 
 export default function NavDrawer() {
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-    // const [state, setState] = useState(false);
-    // const navigateTrigger = useNavigate();
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  // const [state, setState] = useState(false);
+  // const navigateTrigger = useNavigate();
 
-    return (<>
+  return (<>
 
-        <Toolbar>
-            <IconButton
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                onClick={() => { setIsDrawerOpen(true) }
-                } >
+    <Toolbar>
+      <IconButton
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        onClick={() => { setIsDrawerOpen(true) }
+        } >
 
-                <MenuIcon sx={{color:'white'}}/>
+        <MenuIcon sx={{ color: 'white' }} />
 
-            </IconButton>
+      </IconButton>
 
-            <Drawer
-                open={isDrawerOpen}
-                onClose={() => setIsDrawerOpen(false)}
-                // PaperProps allows us to resize the meu
-                PaperProps={{
-                    sx: { width: "25%" }
-                }}>
+      <Drawer
+        open={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+        // PaperProps allows us to resize the meu
+        PaperProps={{
+          sx: { width: "20%" }
+        }}>
 
-                <Typography variant="h6" sx={{ my: 2, textAlign: 'center' }}>
-                    (Menu)
-                </Typography>
+        <Typography variant="h6" sx={{ my: 2, textAlign: 'center' }}>
+          (Menu)
+        </Typography>
 
-                <Divider />
+        <Divider />
 
-                 {/* //THIS SECTION HANDLES MENU BUTTON NAVIGATION  */}
-          {navItems.map((item) => (
-            <ListItem key={item} disablePadding>
+        {/* //THIS SECTION HANDLES MENU BUTTON NAVIGATION  */}
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
 
-              {/* //------------------------------------------------ */}
+            {/* //------------------------------------------------ */}
 
-              {item === 'Home' ? <ListItemButton sx={{ textAlign: 'center' }}
+            {item === 'Home' ? <ListItemButton sx={{ textAlign: 'center' }}
+              component={Link}
+              to='/'
+              onClick={() => {
+                console.info("HOME BUTTON TEST");
+                setIsDrawerOpen(false);
+              }}>
+              <ListItemText primary={item} />
+            </ListItemButton> : ''}
+
+             {/* //------------------------------------------------ */}
+
+             {item === 'Calculator' ? <ListItemButton sx={{ textAlign: 'center' }}
+              component={Link}
+              to='/calculator'
+              onClick={() => {
+                console.info("CALCULATOR BUTTON TEST");
+                setIsDrawerOpen(false);
+              }}>
+              <ListItemText primary={item} />
+            </ListItemButton> : ''}
+
+            {/* //------------------------------------------------ */}
+
+            {item === 'About' ? <ListItemButton sx={{ textAlign: 'center' }}
+              component={Link}
+              to='/about'
+              onClick={() => {
+                console.info("ABOUT BUTTON TEST");
+                setIsDrawerOpen(false);
+              }}>
+              <ListItemText primary={item} />
+            </ListItemButton> : ''}
+
+            {/* //------------------------------------------------ */}
+
+            {item === 'Contact Us' ? <ListItemButton sx={{ textAlign: 'center' }}
+              component={Link}
+              to='/contact'
+              onClick={() => {
+                console.info("CONTACT US BUTTON TEST");
+                setIsDrawerOpen(false);
+              }}>
+              <ListItemText primary={item} />
+            </ListItemButton> : ''}
+
+            {/* //------------------------------------------------ */}
+
+          </ListItem>
+
+        ))};
+
+        {/* //------------------------------------------------ */}
+
+        <Divider />
+
+        {/* //------------------------------------------------ */}
+
+        {navItems2.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'left' }}>
+
+              {item === 'Account' ? <ListItemButton sx={{ textAlign: 'center' }}
                 component={Link}
-                to='/'
-                onClick={() => {
-                  console.info("HOME BUTTON TEST");
-                  setIsDrawerOpen(false);
-                }}>
-                <ListItemText primary={item} />
-              </ListItemButton> : ''}
-
-              {/* //------------------------------------------------ */}
-
-              {item === 'About' ? <ListItemButton sx={{ textAlign: 'center' }}
-                component={Link}
-                to='/about'
-                onClick={() => {
-                  console.info("ABOUT BUTTON TEST");
-                  setIsDrawerOpen(false);
-                }}>
-                <ListItemText primary={item} />
-              </ListItemButton> : ''}
-
-              {/* //------------------------------------------------ */}
-
-              {item === 'Contact Us' ? <ListItemButton sx={{ textAlign: 'center' }}
-                component={Link}
-                to='/contact'
-                onClick={() => {
-                  console.info("CONTACT US BUTTON TEST");
-                  setIsDrawerOpen(false);
-                }}>
-                <ListItemText primary={item} />
-              </ListItemButton> : ''}
-
-              {/* //------------------------------------------------ */}
-
-            </ListItem>
-
-          ))};
-
-          {/* //------------------------------------------------ */}
-
-          <Divider />
-
-          {/* //------------------------------------------------ */}
-
-          {navItems2.map((item) => (
-            <ListItem key={item} disablePadding>
-              <ListItemButton sx={{ textAlign: 'left' }}>
-
-                {item === 'Account' ? <ListItemButton sx={{ textAlign: 'center' }}
-                  component={Link}
                 to='/account'
-                  onClick={() => {
-                    console.info("ACCOUNT BUTTON TEST");
-                    setIsDrawerOpen(false);
-                  }}>
-                  <ListItemText primary={item} />
-                </ListItemButton> : ''}
+                onClick={() => {
+                  console.info("ACCOUNT BUTTON TEST");
+                  setIsDrawerOpen(false);
+                }}>
+                <ListItemText primary={item} />
+              </ListItemButton> : ''}
 
-                {/* //------------------------------------------------ */}
+              {/* //------------------------------------------------ */}
 
-                {item === 'Settings' ? <ListItemButton sx={{ textAlign: 'center' }}
-                  component={Link}
+              {item === 'Settings' ? <ListItemButton sx={{ textAlign: 'center' }}
+                component={Link}
                 to='/settings'
-                  onClick={() => {
-                    console.info("SETTINGS BUTTON TEST");
-                    setIsDrawerOpen(false);
-                  }}>
-                  <ListItemText primary={item} />
-                </ListItemButton> : ''}
+                onClick={() => {
+                  console.info("SETTINGS BUTTON TEST");
+                  setIsDrawerOpen(false);
+                }}>
+                <ListItemText primary={item} />
+              </ListItemButton> : ''}
 
-                {/* //------------------------------------------------ */}
+              {/* //------------------------------------------------ */}
 
-                {item === 'Logout' ? <ListItemButton sx={{ textAlign: 'center' }}
-                  component={Link}
+              {item === 'Logout' ? <ListItemButton sx={{ textAlign: 'center' }}
+                component={Link}
                 to='/'
-                  onClick={() => {
-                    console.info("LOGOUT BUTTON TEST");
-                    setIsDrawerOpen(false);
-                  }}>
-                  <ListItemText primary={item} />
-                </ListItemButton> : ''}
-              </ListItemButton>
-            </ListItem>
-          ))}
+                onClick={() => {
+                  console.info("LOGOUT BUTTON TEST");
+                  setIsDrawerOpen(false);
+                }}>
+                <ListItemText primary={item} />
+              </ListItemButton> : ''}
+            </ListItemButton>
+          </ListItem>
+        ))}
 
-        </Drawer>
-      </Toolbar>
-      <hr style={{ marginLeft: "8px", marginRight: "8px" }} />
+      </Drawer>
+    </Toolbar>
+    <hr style={{ marginLeft: "8px", marginRight: "8px" }} />
 
-    </>
+  </>
   );
 
 
