aeae7b6b61287e97f6b68bd103a854531c12df2b
jkof86 <cisjay@gmail.com>
2024-08-18 18:40:33 -0400

    Redesigned Toolbars and began Calculator Component design

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,21 +1,22 @@
-import { Box } from "@mui/material";
+import { Box, Container, TextField, Toolbar, Typography } from "@mui/material";
 
 export default function About() {
 
     return (<>
-    <center>
-        <Box component="section" sx={{
-            textAlign: 'center', 
+    <Toolbar sx={{
+            flexGrow: 1,
             justifyContent: 'center',
             backgroundColor: 'grey',
-            borderRadius: '25px',
             border: '1px solid black',
             boxShadow: '0px 0px 2px 2px white',
-            margin: '10px',
+            marginBottom: '10px',
             padding: '10px',
-            width: '50%' }}>
-            <h1>About</h1>
-        </Box>
-        </center>
+            width: '100%'
+        }}>
+            <Typography sx={{ fontSize:'20px',
+            fontStyle:'bold'}}>
+                About
+            </Typography>
+        </Toolbar>
     </>);
 }
--- frontend/src/components/Calculator.js
+++ frontend/src/components/Calculator.js
@@ -1,21 +1,78 @@
-import { Box } from "@mui/material";
+import { 
+    Box, Container, TextField, 
+    Toolbar, Typography, InputAdornment } from "@mui/material";
 
 export default function Calculator() {
 
     return (<>
-    <center>
-        <Box component="section" sx={{
-            textAlign: 'center', 
+        <Toolbar sx={{
+            flexGrow: 1,
             justifyContent: 'center',
             backgroundColor: 'grey',
-            borderRadius: '25px',
             border: '1px solid black',
             boxShadow: '0px 0px 2px 2px white',
-            margin: '10px',
+            marginBottom: '10px',
             padding: '10px',
-            width: '50%' }}>
-            <h1>Calorie / Macronutrient Calculator</h1>
-        </Box>
-        </center>
+            width: '100%'
+        }}>
+            <Typography sx={{ fontSize:'20px',
+            fontStyle:'bold'}}>
+                Calorie / Macronutrient Calculator
+            </Typography>
+        </Toolbar>
+        <Container fixed>
+            <Box component='form' sx={{
+                flexGrow:1,
+                justifyContent: 'center',
+                backgroundColor: 'grey',
+                borderRadius: '25px',
+                border: '1px solid black',
+                boxShadow: '0px 0px 2px 2px white',
+                padding: '10px',
+                width: '100%'
+            }}>
+                <TextField id="calorieLimitInput" 
+                label="Calorie Limit"
+                variant="standard" sx={{
+                    color:'white',
+                    margin:'5px'
+                }} InputProps={{
+                    endAdornment: <InputAdornment position='end'>
+                        kcal
+                        </InputAdornment>,
+                  }}/>
+
+                <TextField id="proteinPercentInput" 
+                label="Protein Percentage"
+                variant="standard" sx={{
+                    margin:'5px'
+                }} InputProps={{
+                    endAdornment: <InputAdornment position='end'>
+                        %
+                        </InputAdornment>,
+                  }} />
+
+                <TextField id="carbPercentInput" 
+                label="Carb Percentage"
+                variant="standard" sx={{
+                    margin:'5px'
+                }} InputProps={{
+                    endAdornment: <InputAdornment position='end'>
+                        %
+                        </InputAdornment>,
+                  }} />
+
+                <TextField id="fatPercentInput" 
+                label="Fat Percentage"
+                variant="standard" sx={{
+                    margin:'5px'
+                }} InputProps={{
+                    endAdornment: <InputAdornment position='end'>
+                        %
+                        </InputAdornment>,
+                  }} />
+                
+            </Box>
+        </Container>
     </>);
 }
