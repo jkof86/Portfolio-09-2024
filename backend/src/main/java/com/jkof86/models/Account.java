ced668cb9bfc658e234d3c05d0af99ef8db34918
jkof86 <cisjay@gmail.com>
2024-08-22 14:00:06 -0400

    Removed commented out code from backend

--- backend/src/main/java/com/jkof86/models/Account.java
+++ backend/src/main/java/com/jkof86/models/Account.java
@@ -204,44 +204,5 @@ public class Account {
                 ", fatPercent=" + fatPercent +
                 '}';
     }
-
-    //    private void calculateCalories() {
-//
-//        // prompt user to enter the total daily calorie limit
-//        System.out.print("Enter the total daily calorie limit: ");
-//        Scanner scanner = new Scanner(System.in);
-//
-//        calorieLimit = scanner.nextInt();
-//
-//        // prompt user to enter three macronutrient percentage values
-//        System.out.print("Enter the percentage of protein: ");
-//        int proteinPercentage = scanner.nextInt();
-//        System.out.print("Enter the percentage of carbohydrates: ");
-//        int carbPercentage = scanner.nextInt();
-//        System.out.print("Enter the percentage of fat: ");
-//        int fatPercentage = scanner.nextInt();
-//
-//        if(proteinPercentage + carbPercentage + fatPercentage == 100) {
-//            // calculate the macronutrient values (in grams) given the  entered calorieLimit
-//
-//            //protein has 4 calories per gram
-//            //carbohydrates has 4 calories per gram
-//            //fat has 9 calories per gram
-//            protein = (int) (calorieLimit * proteinPercentage / 100.0 / 4.0);
-//            carbs = (int) (calorieLimit * carbPercentage / 100.0 / 4.0);
-//            fat = (int) (calorieLimit * fatPercentage / 100.0 / 9.0);
-//
-//            System.out.println("-----------------------");
-//
-//            // print the macronutrient values (in grams)
-//            System.out.println("Protein: " + protein + " grams");
-//            System.out.println("Carbohydrates: " + carbs + " grams");
-//            System.out.println("Fat: " + fat + " grams");
-//        }
-//        else {
-//            System.out.println("-----------------------");
-//            System.out.println("Error: Macronutrient % not equal to 100");
-//            System.out.println("Creating person object with default goals...");
-//        }
-//    }
+    
 }
