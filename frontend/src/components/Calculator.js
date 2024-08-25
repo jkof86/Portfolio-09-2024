7be58d4ae801bf7e127c0e481ef198574a34a67b
jkof86 <cisjay@gmail.com>
2024-08-25 18:39:25 -0400

    Working on Textfield inputs, need to fix bug thats forcing concatenation instead of addition during calculation

--- frontend/src/components/Calculator.js
+++ frontend/src/components/Calculator.js
@@ -6,86 +6,151 @@ import {
     TableBody,
     TableRow,
     TableCell,
-    ownerDocument
+
 } from "@mui/material";
 
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 export default function Calculator() {
 
-    const [calorieLimit, setCalorieLimit] = useState(0);
-    const [proteinPercent, setProteinPercent] = useState(0);
-    const [carbPercent, setCarbPercent] = useState(0);
-    const [fatPercent, setFatPercent] = useState(0);
-    var [protein, setProtein] = useState(0);
-    var [carb, setCarb] = useState(0);
-    var [fat, setFat] = useState(0);
+    const [state, setState] = useState({
+        calorieLimit: 0,
+        proteinPercent: 0,
+        carbPercent: 0,
+        fatPercent: 0,
 
-    function handleClick() {
-        // event.preventDefault();
-        // console.log('Calorie Limit: ', calorieLimit,
-        //     'Protein Percent: ', proteinPercent,
-        //     'Carb Percent: ', carbPercent,
-        //     'Fat Percent: ', fatPercent)
+        protein: 0,
+        carb: 0,
+        fat: 0
+    }
+    )
+
+    function handleChange(e) {
+
+        //we handle all input fields here
+        const value = e.target.value;
+        setState({
+            ...state,
+            [e.target.name]: value
+        })
+
+        console.log('Calorie Limit: ', state.calorieLimit,
+            'Carb Percent: ', state.carbPercent,
+            'Protein Percent: ', state.proteinPercent,
+            'Fat Percent: ', state.fatPercent)
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log('Calorie Limit: ', state.calorieLimit,
+            'Carb Percent: ', state.carbPercent,
+            'Protein Percent: ', state.proteinPercent,
+            'Fat Percent: ', state.fatPercent)
 
         // ..code to submit form to backend here...
 
     }
 
-    const calcValid = () => {
-        {
-            // calculate the macronutrient values (in grams) given the  entered calorieLimit
+    const calculateMacros = () => {
 
+        //we create local variables for calculations
+        console.log('Calculate Macros Button Test...');
+        let totalPercent = 0;
+        let carb = state.carbPercent;
+        let protein = state.proteinPercent;
+        const fat = state.fatPercent;
+
+
+        //we calculate the total percent to verify it equals 100%
+        totalPercent = (carb + protein + fat);
+        console.log(typeof (totalPercent));
+        console.log(totalPercent);
+        
+
+        (totalPercent === 100) ? calcValid(protein, carb, fat) : calcInvalid(protein, carb, fat);
+    }
+
+    const calcValid = (protein, carb, fat) => {
+        {
+            // print the macronutrient values (in grams)
+            // console.log("Carbohydrates: " + state.carb.toFixed(2) + " grams");
+            // console.log("Protein: " + state.protein.toFixed(2) + " grams");
+            // console.log("Fat: " + state.fat.toFixed(2) + " grams");
+
+            // we calculate the macronutrient values (in grams) given the  entered calorieLimit
             //fat has 9 calories per gram
-            protein = (calorieLimit * proteinPercent / 100.0 / 4.0);
+            fat = (state.calorieLimit * state.proteinPercent / 100.0 / 4.0);
 
             //carbohydrates has 4 calories per gram
-            carb = (calorieLimit * carbPercent / 100.0 / 4.0);
+            carb = (state.calorieLimit * state.carbPercent / 100.0 / 4.0);
 
             //protein has 4 calories per gram
-            fat = (calorieLimit * fatPercent / 100.0 / 9.0);
+            protein = (state.calorieLimit * state.fatPercent / 100.0 / 9.0);
 
-             // print the macronutrient values (in grams)
-             console.log("Protein: " + protein.toFixed(2) + " grams");
-             console.log("Carbohydrates: " + carb.toFixed(2) + " grams");
-             console.log("Fat: " + fat.toFixed(2) + " grams");
-            
+
+
+            //we reset the states back to 0 
+            //and pass the saved results into the method for displaying
+
+            setState({
+                calorieLimit: 0,
+                proteinPercent: 0,
+                carbPercent: 0,
+                fatPercent: 0,
+
+                protein: 0,
+                carb: 0,
+                fat: 0
+            })
+
+            ShowResults(carb, protein, fat)
         }
 
     }
 
     // if total percent != 100...
-    const calcInvalid = () => { console.log("Error: Macronutrient % not equal to 100"); }
+    const calcInvalid = (protein, carb, fat) => {
+        // display error msg
+        console.log("Error: Macronutrient % not equal to 100");
+        alert("Error: Macronutrient % not equal to 100");
 
-    const calculateMacros = () => {
+        // print the macronutrient values (in grams)
+        console.log("Carbohydrates: " + carb.toFixed(2) + " grams");
+        console.log("Protein: " + protein.toFixed(2) + " grams");
+        console.log("Fat: " + fat.toFixed(2) + " grams");
+    }
 
-        console.log('Calculate Macros Button Test...');
-
-        //we need to cast each value as a number to avoid concatenation
-        var totalPercent = (Number(proteinPercent) + Number(carbPercent) + Number(fatPercent));
-        // console.log(totalPercent);  
-
-        (totalPercent === 100) ? calcValid() : calcInvalid();
-
+    function ShowResults(carb, protein, fat) {
         return (
             <Table sx={{ border: '1px solid black' }}>
                 <TableHead sx={{
                     border: '1px solid black',
                     backgroundColor: 'lightgrey',
                 }}>
-                    <TableCell sx={{ border: '1px solid black', textAlign: 'center' }}>Carbs</TableCell>
-                    <TableCell sx={{ border: '1px solid black', textAlign: 'center' }}>Protein</TableCell>
-                    <TableCell sx={{ border: '1px solid black', textAlign: 'center' }}>Fats</TableCell>
+                    <TableCell sx={{ border: '1px solid black', textAlign: 'center' }}>
+                        Carbs
+                    </TableCell>
+                    <TableCell sx={{ border: '1px solid black', textAlign: 'center' }}>
+                        Protein
+                    </TableCell>
+                    <TableCell sx={{ border: '1px solid black', textAlign: 'center' }}>
+                        Fats</TableCell>
                 </TableHead>
                 <TableBody>
-                    <TableCell sx={{ border: '1px solid black', textAlign: 'center' }}>{protein.toFixed(2)} grams</TableCell>
-                    <TableCell sx={{ border: '1px solid black', textAlign: 'center' }}>{carb.toFixed(2)} grams</TableCell>
-                    <TableCell sx={{ border: '1px solid black', textAlign: 'center' }}>{fat.toFixed(2)} grams</TableCell>
+                    <TableCell sx={{ border: '1px solid black', textAlign: 'center' }}>
+                        {carb.toFixed(2)} grams
+                    </TableCell>
+                    <TableCell sx={{ border: '1px solid black', textAlign: 'center' }}>
+                        {protein.toFixed(2)} grams
+                    </TableCell>
+                    <TableCell sx={{ border: '1px solid black', textAlign: 'center' }}>
+                        {fat.toFixed(2)} grams
+                    </TableCell>
                 </TableBody>
             </Table>
-
         )
-
     }
 
     const ShowGraph = () => {
@@ -107,46 +172,8 @@ export default function Calculator() {
                     </CardMedia>
                     <CardContent>
 
-
                     </CardContent>
                 </CardActionArea>
-            </Card >
-        )
-    }
-
-
-    const ShowResults = () => {
-        return (
-            <Card sx={{
-                // border: '2px solid black',
-                maxWidthidth: '340',
-                borderRadius: '25px',
-                margin: '10px',
-                padding: '10px',
-                textAlign: 'center'
-            }}>
-                <CardActionArea>
-
-                    <CardMedia
-                        component="img"
-                        alt="Macronutrient Image"
-                        image={require("../images/macros.jpg")}
-                    >
-                    </CardMedia>
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Macronutrient Results
-                        </Typography>
-                        {calculateMacros()}
-                    </CardContent>
-
-                </CardActionArea>
-
-                <CardActions>
-                    <Button size="small">Share</Button>
-                    <Button size="small">Learn More</Button>
-                </CardActions>
-
             </Card >
         )
     }
@@ -185,15 +212,20 @@ export default function Calculator() {
             }}>
 
                 {/* for each TextField, we use onInput to store the values */}
+
                 <Grid container spacing={2}>
                     <Grid item xs={3}
                         sx={{ alignContent: 'center' }}>
-                        <TextField id="calorieLimitInput"
-                            required='true'
+                        <TextField
+                            required={true}
                             type="number"
                             label="Calorie Limit"
                             variant="standard"
-                            onInput={e => setCalorieLimit(e.target.value)}
+                            name="calorieLimit"
+                            // value={state.calorieLimit}
+                            onChange={handleChange}
+                            // onInput={e => setCalorieLimit(e.target.value)}
+                            // onSubmit={e => setCalorieLimit(e.target.value)}
                             sx={{
                                 color: 'white',
                                 margin: '5px'
@@ -203,12 +235,14 @@ export default function Calculator() {
                                 </InputAdornment>,
                             }} />
 
-                        <TextField id="carbPercentInput"
-                            required='true'
+                        <TextField
+                            required={true}
                             type="number"
                             label="Carb Percentage"
                             variant="standard"
-                            onInput={e => setCarbPercent(e.target.value)}
+                            name="carbPercent"
+                            value={state.carbPercent}
+                            onChange={handleChange}
                             sx={{
                                 margin: '5px'
                             }} InputProps={{
@@ -217,12 +251,14 @@ export default function Calculator() {
                                 </InputAdornment>,
                             }} />
 
-                        <TextField id="proteinPercentInput"
-                            required='true'
+                        <TextField
+                            required={true}
                             type="number"
                             label="Protein Percentage"
                             variant="standard"
-                            onInput={e => setProteinPercent(e.target.value)}
+                            name="proteinPercent"
+                            value={state.proteinPercent}
+                            onChange={handleChange}
                             sx={{
                                 margin: '5px'
                             }} InputProps={{
@@ -231,12 +267,14 @@ export default function Calculator() {
                                 </InputAdornment>,
                             }} />
 
-                        <TextField id="fatPercentInput"
-                            required='true'
+                        <TextField
+                            required={true}
                             type="number"
                             label="Fat Percentage"
                             variant="standard"
-                            onInput={e => setFatPercent(e.target.value)}
+                            name="fatPercent"
+                            value={state.fatPercent}
+                            onChange={handleChange}
                             sx={{
                                 margin: '5px'
                             }} InputProps={{
@@ -256,17 +294,44 @@ export default function Calculator() {
                             <Button variant="contained"
                                 id="calculateButton"
                                 // type="submit"
-                                onClick={() => {
-                                    { calculateMacros() };
-                                }
-                                } >
+                                onClick={calculateMacros}
+                            >
                                 Calculate
                             </Button>
                         </Box>
-
                     </Grid>
                     <Grid item xs={6}>
-                        {ShowResults()}
+                        <Card sx={{
+                            // border: '2px solid black',
+                            maxWidthidth: '340',
+                            borderRadius: '25px',
+                            margin: '10px',
+                            padding: '10px',
+                            textAlign: 'center'
+                        }}>
+                            <CardActionArea>
+
+                                <CardMedia
+                                    component="img"
+                                    alt="Macronutrient Image"
+                                    image={require("../images/macros.jpg")}
+                                >
+                                </CardMedia>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        Macronutrient Results
+                                    </Typography>
+                                    Test
+                                </CardContent>
+
+                            </CardActionArea>
+
+                            <CardActions>
+                                <Button size="small">Share</Button>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+
+                        </Card >
                     </Grid>
                     <Grid item xs={3}>
                         {ShowGraph()}
