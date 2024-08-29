fc4f5c1ff5193fa421f64dcbf5a9052a2dd77d8d
jkof86 <cisjay@gmail.com>
2024-08-29 17:36:08 -0400

    Installed Chart.js data labels module

--- frontend/src/components/PieChart.js
+++ frontend/src/components/PieChart.js
@@ -1,48 +1,59 @@
 import React from 'react';
 import { Pie } from 'react-chartjs-2';
-import {Chart, ArcElement} from 'chart.js'
+import { Chart, ArcElement } from 'chart.js'
 
-export default function PieChart({carb, protein, fat}) {
+export default function PieChart({ carb, protein, fat }) {
     Chart.register(ArcElement);
 
     const data = {
-      labels: [
-        'Red', 'Blue', 'Green', 
-        // 'Yellow', 'Purple', 'Orange'
-    ],
-      datasets: [
-        {
-          label: '# of Votes',
-        //   data: [40, 40, 20],
-        data: [carb, protein, fat],
+        labels: [
+            'Red', 'Blue', 'Green',
+            // 'Yellow', 'Purple', 'Orange'
+        ],
+        datasets: [
+            {
+                label: '# of Votes',
+                //   data: [40, 40, 20],
+                data: [carb, protein, fat],
 
-          backgroundColor: [
-            'red',
-            'blue',
-            'green',
-            // 'rgba(255, 99, 132, 0.2)',
-            // 'rgba(54, 162, 235, 0.2)',
-            // 'rgba(75, 192, 192, 0.2)',
-            // 'rgba(255, 206, 86, 0.2)',
-            // 'rgba(153, 102, 255, 0.2)',
-            // 'rgba(255, 159, 64, 0.2)',
-          ],
-          borderColor: [
-            'white',
-            'white',
-            'white'
-            // 'rgba(255, 99, 132, 1)',
-            // 'rgba(54, 162, 235, 1)',
-            // 'rgba(75, 192, 192, 1)',
-            // 'rgba(255, 206, 86, 1)',
-            // 'rgba(153, 102, 255, 1)',
-            // 'rgba(255, 159, 64, 1)',
-          ],
-          borderWidth: 1,
+                backgroundColor: [
+                    'red',
+                    'blue',
+                    'green',
+                    // 'rgba(255, 99, 132, 0.2)',
+                    // 'rgba(54, 162, 235, 0.2)',
+                    // 'rgba(75, 192, 192, 0.2)',
+                    // 'rgba(255, 206, 86, 0.2)',
+                    // 'rgba(153, 102, 255, 0.2)',
+                    // 'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                    'white',
+                    'white',
+                    'white'
+                    // 'rgba(255, 99, 132, 1)',
+                    // 'rgba(54, 162, 235, 1)',
+                    // 'rgba(75, 192, 192, 1)',
+                    // 'rgba(255, 206, 86, 1)',
+                    // 'rgba(153, 102, 255, 1)',
+                    // 'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1
+            }],
+
+        options: {
+            plugins: {
+                datalabels: {
+                    color: 'white',
+                    font: {
+                        size: 14,
+                        weight: 'bold'
+                    },
+                }
+            }
         },
-      ],
-    };
-  
+    }
+
+
     return <Pie data={data} />;
-  };
-  
+};
