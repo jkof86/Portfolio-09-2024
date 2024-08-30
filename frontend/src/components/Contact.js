c9093a86fab55ab401a881766c438bd4cb7084b9
jkof86 <cisjay@gmail.com>
2024-08-30 17:45:44 -0400

    Updated WebApp Title / added icon images / began working on Contact Component design

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,4 +1,8 @@
-import { Box, Container, Grid, Divider, Toolbar, Typography } from "@mui/material";
+import {
+    Box, Container, Toolbar, Typography,
+    Button, Grid, Card, CardActions, CardActionArea,
+    CardContent, CardMedia
+} from "@mui/material";
 
 export default function Contact() {
 
@@ -13,9 +17,11 @@ export default function Contact() {
             padding: '10px',
             width: '100%'
         }}>
-            <Typography sx={{ fontSize:'20px',
-            fontStyle:'bold'}}>
-                Contact Us
+            <Typography sx={{
+                fontSize: '20px',
+                fontStyle: 'bold'
+            }}>
+                Contact Information
             </Typography>
         </Toolbar>
 
@@ -39,21 +45,148 @@ export default function Contact() {
                 </center>
 
                 <Grid container spacing={2}>
-                    <Grid item xs={3}>
 
+                    <Grid item xs={6}>
+
+                        <center>
+                            <Card sx={{
+                                // border: '2px solid black',
+                                maxWidth: '50%',
+                                borderRadius: '25px',
+                                margin: '10px',
+                                padding: '10px',
+                                textAlign: 'center',
+                            }}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        alt="LinkedIn Icon"
+                                        image={require("../images/icons/linkedIn_PNG8.png")}
+                                    >
+                                    </CardMedia>
+                                    <CardContent >
+
+                                    </CardContent>
+                                </CardActionArea>
+
+                                <CardActions>
+                                    <Button size="small">Share</Button>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
+
+                            </Card >
+                        </center>
 
                     </Grid>
-                       
-                        <Divider sx={{
-                            marginTop: '10px',
-                            marginBottom: '10px',
-                            borderBottomWidth: '2px',
-                            borderColor: 'black'
-                        }} />
 
-                        
+                    {/********************************************************/}
+
+                    <Grid item xs={6}>
+
+                        <center>
+                            <Card sx={{
+                                // border: '2px solid black',
+                                maxWidth: '50%',
+                                borderRadius: '25px',
+                                margin: '10px',
+                                padding: '10px',
+                                textAlign: 'center',
+                            }}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        alt="Facebook Icon"
+                                        image={require("../images/icons/Facebook_Icon_(Official_2).png")}
+                                    >
+                                    </CardMedia>
+                                    <CardContent >
+
+                                    </CardContent>
+                                </CardActionArea>
+
+                                <CardActions>
+                                    <Button size="small">Share</Button>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
+
+                            </Card >
+                        </center>
+
                     </Grid>
-            
+
+                    {/********************************************************/}
+
+                    <Grid item xs={6}>
+
+                        <center>
+                            <Card sx={{
+                                // border: '2px solid black',
+                                maxWidth: '50%',
+                                borderRadius: '25px',
+                                margin: '10px',
+                                padding: '10px',
+                                textAlign: 'center',
+                            }}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        alt="Instagram Icon"
+                                        image={require("../images/icons/Instagram_icon.png")}
+                                    >
+                                    </CardMedia>
+                                    <CardContent >
+
+                                    </CardContent>
+                                </CardActionArea>
+
+                                <CardActions>
+                                    <Button size="small">Share</Button>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
+
+                            </Card >
+                        </center>
+
+                    </Grid>
+
+                    {/********************************************************/}
+
+                    <Grid item xs={6}>
+
+                        <center>
+                            <Card sx={{
+                                // border: '2px solid black',
+                                maxWidth: '50%',
+                                borderRadius: '25px',
+                                margin: '10px',
+                                padding: '10px',
+                                textAlign: 'center',
+                            }}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        alt="Youtube Icon"
+                                        image={require("../images/icons/youtube-transparent-png-15.png")}
+                                    >
+                                    </CardMedia>
+                                    <CardContent >
+
+                                    </CardContent>
+                                </CardActionArea>
+
+                                <CardActions>
+                                    <Button size="small">Share</Button>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
+
+                            </Card >
+                        </center>
+
+                    </Grid>
+
+                    {/********************************************************/}
+
+                </Grid>
             </Box>
         </Container >
     </>);
