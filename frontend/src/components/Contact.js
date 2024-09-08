fe12cf2576ff7f4a9716bd1f877192775d70a0dd
jkof86 <cisjay@gmail.com>
2024-09-08 15:02:13 -0400

    Continued gui adjustments for mobile view / adjusted Contact component icon size

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -28,198 +28,199 @@ export default function Contact() {
         {/********************************************************/}
 
         {/* <Container disableGutters={false} sx={{ width: '100vw' }}> */}
-        <Box padding={0} sx={{
-            justifyContent: 'center',
-            backgroundColor: 'white',
-            borderRadius: '25px',
-            border: '1px solid black',
-            boxShadow: '0px 0px 2px 2px white',
-            padding: '10px',
-            margin: '20px',
-            width: '100vw'
-        }}>
-            <center>
-                <SlideShow />
-            </center>
+        <center>
+            <Box padding={0} sx={{
+                justifyContent: 'center',
+                backgroundColor: 'white',
+                borderRadius: '25px',
+                border: '1px solid black',
+                boxShadow: '0px 0px 2px 2px white',
+                padding: '10px',
+                margin: '20px',
+                width: '75vw'
+            }}>
+                <center>
+                    <SlideShow />
+                </center>
 
-            <Grid container spacing={4} sx={{ justifyContent: 'center' }}>
+                <Grid container spacing={4} sx={{ justifyContent: 'center' }}>
 
-                <Grid item xs={2} >
+                    <Grid item xs={2} >
 
-                    <center>
-                        <Card sx={{
-                            // border: '2px solid black',
-                            display: 'flex',
-                            minWidth: '100%',
-                            borderRadius: '25px',
-                            padding: '5px',
-                            margin: '20px',
-                            textAlign: 'center'
-                        }}>
-                            <CardActionArea component={Link} to='https://www.linkedin.com/in/jkof86' target="_blank" >
-                                <CardMedia
-                                    component="img"
-                                    alt="LinkedIn Icon"
-                                    image={require("../images/icons/linkedIn_PNG8.png")}
-                                >
-                                </CardMedia>
-                                {/* <CardContent >
+                        <center>
+                            <Card sx={{
+                                // border: '2px solid black',
+                                display: 'flex',
+                                minWidth: '75%',
+                                borderRadius: '25px',
+                                padding: '5px',
+                                margin: '20px',
+                                textAlign: 'center'
+                            }}>
+                                <CardActionArea component={Link} to='https://www.linkedin.com/in/jkof86' target="_blank" >
+                                    <CardMedia
+                                        component="img"
+                                        alt="LinkedIn Icon"
+                                        image={require("../images/icons/linkedIn_PNG8.png")}
+                                    >
+                                    </CardMedia>
+                                    {/* <CardContent >
 
                                     </CardContent> */}
-                            </CardActionArea>
+                                </CardActionArea>
 
-                            {/* <CardActions>
+                                {/* <CardActions>
                                     <Button size="small">Share</Button>
                                     <Button size="small">Learn More</Button>
                                 </CardActions> */}
 
-                        </Card >
-                    </center>
+                            </Card >
+                        </center>
 
-                </Grid>
+                    </Grid>
 
-                {/********************************************************/}
+                    {/********************************************************/}
 
-                <Grid item xs={2} >
+                    <Grid item xs={2} >
 
-                    <center>
-                        <Card sx={{
-                            // border: '2px solid black',
-                            minWidth: '100%',
-                            borderRadius: '25px',
-                            padding: '5px',
-                            margin: '20px',
-                            textAlign: 'center',
-                        }}>
-                            <CardActionArea component={Link} to='https://github.com/jkof86' target="_blank">
-                                <CardMedia
-                                    component="img"
-                                    alt="GitHub Icon"
-                                    image={require("../images/icons/github_PNG45.png")}
-                                >
-                                </CardMedia>
-                                {/* <CardContent >
+                        <center>
+                            <Card sx={{
+                                // border: '2px solid black',
+                                minWidth: '75%',
+                                borderRadius: '25px',
+                                padding: '5px',
+                                margin: '20px',
+                                textAlign: 'center',
+                            }}>
+                                <CardActionArea component={Link} to='https://github.com/jkof86' target="_blank">
+                                    <CardMedia
+                                        component="img"
+                                        alt="GitHub Icon"
+                                        image={require("../images/icons/github_PNG45.png")}
+                                    >
+                                    </CardMedia>
+                                    {/* <CardContent >
 
                                     </CardContent> */}
-                            </CardActionArea>
+                                </CardActionArea>
 
-                            {/* <CardActions>
+                                {/* <CardActions>
                                     <Button size="small">Share</Button>
                                     <Button size="small">Learn More</Button>
                                 </CardActions> */}
 
-                        </Card >
-                    </center>
+                            </Card >
+                        </center>
 
-                </Grid>
+                    </Grid>
 
-                {/********************************************************/}
+                    {/********************************************************/}
 
-                <Grid item xs={2} >
+                    <Grid item xs={2} >
 
-                    <center>
-                        <Card sx={{
-                            // border: '2px solid black',
-                            minWidth: '100%',
-                            borderRadius: '25px',
-                            padding: '5px',
-                            margin: '20px',
-                            textAlign: 'center',
-                        }}>
-                            <CardActionArea component={Link} to='https://www.facebook.com/jason.kofi.1' target="_blank">
-                                <CardMedia
-                                    component="img"
-                                    alt="Facebook Icon"
-                                    image={require("../images/icons/Facebook_Icon_(Official_2).png")}
-                                >
-                                </CardMedia>
-                                {/* <CardContent >
+                        <center>
+                            <Card sx={{
+                                // border: '2px solid black',
+                                minWidth: '75%',
+                                borderRadius: '25px',
+                                padding: '5px',
+                                margin: '20px',
+                                textAlign: 'center',
+                            }}>
+                                <CardActionArea component={Link} to='https://www.facebook.com/jason.kofi.1' target="_blank">
+                                    <CardMedia
+                                        component="img"
+                                        alt="Facebook Icon"
+                                        image={require("../images/icons/Facebook_Icon_(Official_2).png")}
+                                    >
+                                    </CardMedia>
+                                    {/* <CardContent >
 
                                     </CardContent> */}
-                            </CardActionArea>
+                                </CardActionArea>
 
-                            {/* <CardActions>
+                                {/* <CardActions>
                                     <Button size="small">Share</Button>
                                     <Button size="small">Learn More</Button>
                                 </CardActions> */}
 
-                        </Card >
-                    </center>
+                            </Card >
+                        </center>
 
-                </Grid>
+                    </Grid>
 
-                {/********************************************************/}
+                    {/********************************************************/}
 
-                <Grid item xs={2} >
+                    <Grid item xs={2} >
 
-                    <center>
-                        <Card sx={{
-                            // border: '2px solid black',
-                            minWidth: '100%',
-                            borderRadius: '25px',
-                            padding: '5px',
-                            margin: '20px',
-                            textAlign: 'center',
-                        }}>
-                            <CardActionArea component={Link} to='https://www.instagram.com/jkof86/' target="_blank">
-                                <CardMedia
-                                    component="img"
-                                    alt="Instagram Icon"
-                                    image={require("../images/icons/Instagram_icon.png")}
-                                >
-                                </CardMedia>
-                                {/* <CardContent >
+                        <center>
+                            <Card sx={{
+                                // border: '2px solid black',
+                                minWidth: '75%',
+                                borderRadius: '25px',
+                                padding: '5px',
+                                margin: '20px',
+                                textAlign: 'center',
+                            }}>
+                                <CardActionArea component={Link} to='https://www.instagram.com/jkof86/' target="_blank">
+                                    <CardMedia
+                                        component="img"
+                                        alt="Instagram Icon"
+                                        image={require("../images/icons/Instagram_icon.png")}
+                                    >
+                                    </CardMedia>
+                                    {/* <CardContent >
 
                                     </CardContent> */}
-                            </CardActionArea>
+                                </CardActionArea>
 
-                            {/* <CardActions>
+                                {/* <CardActions>
                                     <Button size="small">Share</Button>
                                     <Button size="small">Learn More</Button>
                                 </CardActions> */}
 
-                        </Card >
-                    </center>
+                            </Card >
+                        </center>
 
-                </Grid>
+                    </Grid>
 
-                {/********************************************************/}
+                    {/********************************************************/}
 
-                <Grid item xs={2} >
+                    <Grid item xs={2} >
 
-                    <center>
-                        <Card sx={{
-                            // border: '2px solid black',
-                            minWidth: '100%',
-                            borderRadius: '25px',
-                            padding: '5px',
-                            margin: '20px',
-                            textAlign: 'center',
-                        }}>
-                            <CardActionArea component={Link} to='https://www.youtube.com/@jkof86' target="_blank">
-                                <CardMedia
-                                    component="img"
-                                    alt="Youtube Icon"
-                                    image={require("../images/icons/youtube-transparent-png-15.png")}
-                                >
-                                </CardMedia>
-                                {/* <CardContent >
+                        <center>
+                            <Card sx={{
+                                // border: '2px solid black',
+                                minWidth: '75%',
+                                borderRadius: '25px',
+                                padding: '5px',
+                                margin: '20px',
+                                textAlign: 'center',
+                            }}>
+                                <CardActionArea component={Link} to='https://www.youtube.com/@jkof86' target="_blank">
+                                    <CardMedia
+                                        component="img"
+                                        alt="Youtube Icon"
+                                        image={require("../images/icons/youtube-transparent-png-15.png")}
+                                    >
+                                    </CardMedia>
+                                    {/* <CardContent >
 
                                     </CardContent> */}
-                            </CardActionArea>
+                                </CardActionArea>
 
-                            {/* <CardActions>
+                                {/* <CardActions>
                                     <Button size="small">Share</Button>
                                     <Button size="small">Learn More</Button>
                                 </CardActions> */}
 
-                        </Card >
-                    </center>
+                            </Card >
+                        </center>
 
-                </Grid>
+                    </Grid>
 
-                {/********************************************************/}
-                {/* 
+                    {/********************************************************/}
+                    {/* 
                     <Grid item xs={3}>
 
                         <center>
@@ -232,26 +233,28 @@ export default function Contact() {
                                 textAlign: 'center',
                             }}>
                                 {/* <CardActionArea component={Link} to='https://www.youtube.com/@jkof86' target="_blank"> */}
-                {/* <CardMedia>
+                    {/* <CardMedia>
 
                                     </CardMedia> */}
-                {/* <CardContent > */}
-                {/* <PDFViewer /> */}
-                {/* </CardContent> */}
-                {/* </CardActionArea> */}
+                    {/* <CardContent > */}
+                    {/* <PDFViewer /> */}
+                    {/* </CardContent> */}
+                    {/* </CardActionArea> */}
 
-                {/* <CardActions>
+                    {/* <CardActions>
                                     <Button size="small">Share</Button>
                                     <Button size="small">Learn More</Button>
                                     </CardActions> */}
 
-                {/* </Card > */}
-                {/* </center> */}
+                    {/* </Card > */}
+                    {/* </center> */}
 
-                {/* </Grid> */}
-            </Grid>
+                    {/* </Grid> */}
+                </Grid>
 
-        </Box>
+            </Box>
+        </center>
+
         {/* </Container > */}
     </>);
 }
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,21 +1,26 @@
-import { Box } from "@mui/material";
+import { Box, Container, Toolbar } from "@mui/material";
 
 import banner from '../images/photos/banner03.png';
 
 export default function Home() {
 
     return (<>
-        <Box sx={{
-            backgroundImage: `url(${banner})`,
-            backgroundSize: 'cover',
-            borderRadius: '25px',
-            border: '1px solid black',
-            boxShadow: '0px 0px 2px 2px white',
-            minHeight: '200px'
-        }}
-        />
-
         <center>
+            <Toolbar sx={{
+                backgroundImage: `url(${banner})`,
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                backgroundColor: 'white',
+                borderRadius: '25px',
+                border: '1px solid black',
+                boxShadow: '0px 0px 2px 2px white',
+                height: '200px',
+                maxHeight: '200px',
+                width: '700px'
+            }}
+            />
+
             <Box padding={0} sx={{
                 justifyContent: 'center',
                 backgroundColor: 'white',
@@ -24,7 +29,7 @@ export default function Home() {
                 boxShadow: '0px 0px 2px 2px white',
                 padding: '20px',
                 margin: '20px',
-                width: '75vw',
+                width: '50vw',
                 height: '85vh'
             }}>
                 <h1>Home</h1>
